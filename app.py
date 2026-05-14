943a883438339eb513e3e39a36b26d63536b2efd
Kleber <klebernunesdeassis0@gmail.com>
2026-05-14 16:43:10 -0300

    Primeiro sistema Flask

--- app.py
+++ app.py
@@ -1,53 +1,91 @@
-from flask import Flask ,render_template, request
+from flask import Flask, render_template, request,redirect,session
 
-app = Flask(__name__)
+app =  Flask(__name__)
+
+app.secret_key = "123456"
+
+EMAIL = "admin@gmail.com"
+SENHA = "123"
+
 
 @app.route("/")
-def home():
+def login():
     return render_template("index.html")
+
+@app.route("/login", methods=["post"])
+def logar():
+
+    email = request.form["email"]
+    senha = request.form["senha"]
+
+    if EMAIL == email and SENHA==senha:
+        session["usuario"] = email
+        return redirect("/calculadora")
+    else:
+        return"Email ou senha inválidos"
+
+@app.route("/calculadora")
+def calculadora():
+
+    if "usuario" in session:
+
+        return render_template("Calculadora.html")
+
+    else:
+        return redirect("/")
+    
+
+@app.route("/logout")
+def logout():
+    session.pop("usuario")
+
+    return redirect("/")
 
 @app.route("/somar", methods=["post"])
 def somar():
 
-    n1 = int(request.form["n1"])
-    n2 = int(request.form["n2"])
+    n1=int(request.form["n1"])
+    n2=int(request.form["n2"])
 
-    resultado1 = n1+n2
-   
-    return f"O resultado da soma de n1+n2 é {resultado1}"
+    resultado = n1+n2
 
-@app.route("/subtrair" , methods=["post"])
+    return f"A soma dos resultados é {resultado}"
+
+@app.route("/subtrair", methods=["post"])
 def subtrair():
 
-    n1 = int(request.form["n1"])
-    n2 = int(request.form["n2"])
+    n1=int(request.form["n1"])
+    n2=int(request.form["n2"])
 
-    resultado2 = n1-n2
-   
-    return f"O resultado da subtração de n1-n2 é {resultado2}"
+    resultado = n1-n2
 
-@app.route("/multiplicar" , methods=["post"])
+    return f"A soma dos resultados é {resultado}"
+
+@app.route("/multiplicar", methods=["post"])
 def multiplicar():
 
-    n1 = int(request.form["n1"])
-    n2 = int(request.form["n2"])
+    n1=int(request.form["n1"])
+    n2=int(request.form["n2"])
 
-    resultado3 = n1*n2
-   
-    return f"O resultado da soma de n1*n2 é {resultado3}"
+    resultado = n1*n2
 
-
-@app.route("/dividir" , methods=["post"])
+    return f"A soma dos resultados é {resultado}"
+    
+@app.route("/dividir", methods=["post"])
 def dividir():
 
-    n1 = int(request.form["n1"])
-    n2 = int(request.form["n2"])
+    n1=int(request.form["n1"])
+    n2=int(request.form["n2"])
 
-    resultado4 = n1/n2
-   
-    return f"O resultado da soma de n1+n2 é {resultado4}"
+  
+    if n2 == 0:
+        return "Não é possível divdr por zero"
+
+    resultado = n1/n2
+
+    return f"A soma dos resultados é {resultado}"
+    
 
 
-
-if __name__==("__main__"):
+if __name__=="__main__":
     app.run(debug=True)
